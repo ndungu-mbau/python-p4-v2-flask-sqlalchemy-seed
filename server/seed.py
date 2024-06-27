ffcfcc1fb84c5206e79d8ef2f74f53c645c4beda
Nelson Mbau <mbau.ndungu@gmail.com>
2024-06-27 09:53:06 +0300

    feat: add code to seed db, make migrations

--- server/seed.py
+++ server/seed.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
-#server/seed.py
+# server/seed.py
 
 from app import app
 from models import db, Pet
 
-with app.app_context():
+from faker import Faker
+from random import choice as rc
 
+fake = Faker()
+
+with app.app_context():
+    Pet.query.delete()
+
+    # Species Types
+    species = ["cat", "dog", "hamster", "snake"]
     # Create an empty list
     pets = []
 
     # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
+    for _ in range(10):
+        pets.append(Pet(name=fake.first_name(), species=rc(species)))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
